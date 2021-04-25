7bae725d6aa9e535d444c0cb69bae2b722c1e588
Ralf Brait Lehepuu <ralf.lehepuu@gmail.com>
2021-04-25 11:15:47 +0300

    Ui and Hiding
    Lol is hidden

--- Assets/Brait/Scripts/Jelly.cs
+++ Assets/Brait/Scripts/Jelly.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Jelly : MonoBehaviour
 {
     [SerializeField] float PushForce = 10f;
-    [SerializeField] float jumpTime = 10f;
+    [SerializeField] float jumpTime = 5f;
 
     [SerializeField] float slowForce = 0.01f;
     [SerializeField] float offset = 60f;
@@ -14,11 +14,18 @@ public class Jelly : MonoBehaviour
 
     private float lastJump = 0;
     private Vector3 lastJumpDir;
+    private int toKill = 1;
+
+    private void Start()
+    {
+        jumpTime += Random.Range(-1f, 1f);
+    }
 
     private void FixedUpdate()
     {
         if (jumpTime < Time.time - lastJump)
         {
+            toKill = 1;
             lastJump = Time.time;
 
             lastJumpDir = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f),0).normalized * PushForce;
@@ -30,5 +37,14 @@ public class Jelly : MonoBehaviour
         float rotation = (float)(Mathf.Atan2(velo.y, velo.x) / (2 * Mathf.PI) * 360);
 
         transform.rotation = Quaternion.Euler(0, 0, rotation-offset);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag  == "Fish" && toKill != 0)
+        {
+            Destroy(collision.gameObject);
+            toKill = 0;
+        }
     }
 }
